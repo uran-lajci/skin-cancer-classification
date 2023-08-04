64d631baf4f6f5e97ba4f0dcde638f5d1396bba9
uranlajci <117693854+uran-lajci@users.noreply.github.com>
2023-08-04 10:34:11 +0200

    updated the cnn model

--- model_training/cnn.py
+++ model_training/cnn.py
@@ -9,15 +9,23 @@ warnings.filterwarnings("ignore")
 
 
 def load_metadata(file_path):
-    """Loads and preprocesses metadata"""
     metadata = pd.read_csv(file_path)
+    metadata = metadata[metadata["lesion_type"] != -1]
     metadata['image_id'] = metadata['image_id'].apply(lambda x: f'{x}.jpg')
-    metadata["lesion_type"] = metadata["lesion_type"].apply(lambda x: f'{x}')
+    lesion_type_mapping = {
+        0: "BKL",
+        1: "NV",
+        2: "DF",
+        3: "MEL",
+        4: "VASC",
+        5: "BCC",
+        6: "AKIEC"
+    }
+    metadata['lesion_type'] = metadata['lesion_type'].map(lesion_type_mapping)
     return metadata
 
 
 def get_datagen():
-    """Creates and returns an ImageDataGenerator"""
     datagen = ImageDataGenerator(
         rescale=1./255.,
         validation_split=0.2,
@@ -30,47 +38,43 @@ def get_datagen():
     return datagen
 
 
-def get_generator(datagen, dataframe, directory, subset):
-    """Creates and returns a generator for training or validation data"""
-    generator = datagen.flow_from_dataframe(
-        dataframe=dataframe,
-        directory=directory,
-        x_col="image_id",
-        y_col="lesion_type",
-        subset=subset,
-        batch_size=32,
-        seed=42,
-        shuffle=True,
-        class_mode="categorical",
-        target_size=(64,64))
-    return generator
+def get_generator(datagen, dataframe, directory):
+    return datagen.flow_from_dataframe(
+            dataframe=dataframe,  
+            directory=directory, 
+            x_col="image_id",
+            y_col="lesion_type",
+            batch_size=32, 
+            seed=42,
+            shuffle=True,
+            class_mode="categorical",
+            target_size=(128, 128))  
 
 
 def build_model():
-    """Builds and returns the CNN model"""
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)))
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3))) 
+    model.add(MaxPooling2D((2, 2))) 
+    model.add(Conv2D(64, (3, 3), activation='relu')) 
     model.add(MaxPooling2D((2, 2)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu')) 
     model.add(MaxPooling2D((2, 2)))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(Flatten())
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(7, activation='softmax')) 
-    model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
 
 if __name__ == "__main__":
     metadata = load_metadata("dataset/metadata.csv")
+    
     datagen = get_datagen()
 
-    train_generator = get_generator(datagen, metadata, "dataset/images/train", "training")
-    valid_generator = get_generator(datagen, metadata, "dataset/images/validation", "validation")
-
+    train_generator = get_generator(datagen, metadata, "dataset/images/train")
+    valid_generator = get_generator(datagen, metadata, "dataset/images/validation")
+     
     model = build_model()
 
     model.fit(train_generator,
@@ -78,6 +82,6 @@ if __name__ == "__main__":
           steps_per_epoch=train_generator.n//train_generator.batch_size,
           validation_steps=valid_generator.n//valid_generator.batch_size,
           epochs=10)
-
+    
     model.save("model_training/models/cnn_model.h5")
     print("Saved the cnn model.")
